c74e6b0654fd44e1869f7380ba215fbb1bd4dcae
simone <sorzesimone1@gmail.com>
2021-02-10 21:15:43 +0100

    connecting process to mainscript

--- script.py
+++ script.py
@@ -4,43 +4,11 @@
 # Author: simo54
 # -----------------------------------------------------------
 
-import os
-import subprocess
-
-lsCmd = ["ls"]
-
-dirOne = "yourdirectory/"
-
-errorOne = "❌ Ops, an error occurred ❌"
-
-# -----------------------------------------------------------
-processOne = subprocess.Popen(lsCmd, stdout=subprocess.PIPE)
-
-while processOne.poll() is None:
-    output = processOne.stdout.readline()
-    outputDecode = output.strip()
-    if processOne.poll() is not None and output == '':
-        break
-    if output:
-        print(outputDecode.decode())  # decode() is used to remove b prefix
-exitCodeProcessOne = processOne.poll()
-
-print("Process Complete 🦸 \n")
-
-if exitCodeProcessOne != 0:
-    print(errorOne)
-    exit()
-else:
-    print("🙌 Exit with code 0 🙌")
-# -----------------------------------------------------------
-
-# =============== Changing Directory =============== #
-# os.chdir(dirOne)
-# =============== Changing Directory =============== #
+from tasks import *
 
 
 def main():
-    print("ok")
+    test()
 
 
 if __name__ == "__main__":
