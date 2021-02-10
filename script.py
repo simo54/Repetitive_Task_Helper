199e4ac4baabbf751ce8db0e69219f0496fd9535
simone <sorzesimone1@gmail.com>
2021-02-10 21:38:05 +0100

    formatting output .log

--- script.py
+++ script.py
@@ -8,7 +8,8 @@ from tasks import *
 
 
 def main():
-    logging.basicConfig(filename='test/myapp.log', level=logging.INFO)
+    logging.basicConfig(filename='test/myapp.log', level=logging.DEBUG,
+                        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S',)
     logging.info('Started')
 
     test()
